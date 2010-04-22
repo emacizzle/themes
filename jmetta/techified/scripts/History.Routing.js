e96e39a853b203c2c0eafd5df22c2a9d12526fea
mettadore <john@mettadore.com>
2010-04-22 10:34:44 -0700

    Update pages with changes, gallery timing

--- jmetta/techified/scripts/History.Routing.js
+++ jmetta/techified/scripts/History.Routing.js
@@ -1,11 +1,11 @@
-/**
- * History.Routing
- *
- * @version		2.0
- *
- * @license		MIT License
- * @author		Harald Kirschner <mail [at] digitarald.de>
- * @copyright	2008 Author
- */
-
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6.r(m s());6.r({7:{t:\';\'},g:[],u:3(a){8(2.g.z(a)==1)2.A(\'B\',2.h)},v:3(a){2.g.C(a)},h:3(a,b,c){8(!c)2.g.D(n.o(\'h\',2.E))},9:3(){4 2.g.F(n.o(\'9\')).G().H(2.7.t)},p:3(){4 2.I(2.9())}});6.J=m K({L:[M,s],7:{N:O,d:[],e:q,w:\'\',9:3(a){4 a[0]},x:$P},Q:3(a){2.R(a);2.e=2.7.e||\'(.*)\';8($S(2.e)==\'T\')2.e=m U(2.e,2.7.w);2.5=2.d=2.7.d.k();6.u(2);4 2},V:3(a){8(2.5.y()==a.y())4 2;2.5=a;6.p();4 2},W:3(a,b){8(2.5[a]==b)4 2;2.5[a]=b;6.p();4 2},X:3(a){f b=2.5.k();2.5=a;f c=6.9();2.5=b;4 c},Y:3(){6.v(2)},9:3(){8(2.7.Z&&(l(2.5)==l(2.d)))4 q;4 2.7.9.10(2,2.5)},h:3(a){f b=a.h(2.e);f c=2.d;8(b){b.11(0,1);12(f i=0,j=b.13;i<j;i++)b[i]=$14(b[i],c[i]||q);8(l(b)!=l(c))2.5=b}15{2.5=2.d.k()}2.16(\'x\',[2.5,2.d])}});n.o=3(b){f c=17.k(18,1);4 3(a){4 a[b].19(a,c)}};',62,72,'||this|function|return|values|History|options|if|generate||||defaults|pattern|var|routes|match|||slice|String|new|Function|methodize|update|null|implement|Options|separator|register|unregister|flags|onMatch|toString|push|addEvent|changed|remove|each|state|map|clean|join|setState|Route|Class|Implements|Events|skipDefaults|true|empty|initialize|setOptions|type|string|RegExp|setValues|setValue|build|destroy|skipDefaultMatch|call|splice|for|length|pick|else|fireEvent|Array|arguments|apply'.split('|'),0,{}))
+/**
+ * History.Routing
+ *
+ * @version		2.0
+ *
+ * @license		MIT License
+ * @author		Harald Kirschner <mail [at] digitarald.de>
+ * @copyright	2008 Author
+ */
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6.r(m s());6.r({7:{t:\';\'},g:[],u:3(a){8(2.g.z(a)==1)2.A(\'B\',2.h)},v:3(a){2.g.C(a)},h:3(a,b,c){8(!c)2.g.D(n.o(\'h\',2.E))},9:3(){4 2.g.F(n.o(\'9\')).G().H(2.7.t)},p:3(){4 2.I(2.9())}});6.J=m K({L:[M,s],7:{N:O,d:[],e:q,w:\'\',9:3(a){4 a[0]},x:$P},Q:3(a){2.R(a);2.e=2.7.e||\'(.*)\';8($S(2.e)==\'T\')2.e=m U(2.e,2.7.w);2.5=2.d=2.7.d.k();6.u(2);4 2},V:3(a){8(2.5.y()==a.y())4 2;2.5=a;6.p();4 2},W:3(a,b){8(2.5[a]==b)4 2;2.5[a]=b;6.p();4 2},X:3(a){f b=2.5.k();2.5=a;f c=6.9();2.5=b;4 c},Y:3(){6.v(2)},9:3(){8(2.7.Z&&(l(2.5)==l(2.d)))4 q;4 2.7.9.10(2,2.5)},h:3(a){f b=a.h(2.e);f c=2.d;8(b){b.11(0,1);12(f i=0,j=b.13;i<j;i++)b[i]=$14(b[i],c[i]||q);8(l(b)!=l(c))2.5=b}15{2.5=2.d.k()}2.16(\'x\',[2.5,2.d])}});n.o=3(b){f c=17.k(18,1);4 3(a){4 a[b].19(a,c)}};',62,72,'||this|function|return|values|History|options|if|generate||||defaults|pattern|var|routes|match|||slice|String|new|Function|methodize|update|null|implement|Options|separator|register|unregister|flags|onMatch|toString|push|addEvent|changed|remove|each|state|map|clean|join|setState|Route|Class|Implements|Events|skipDefaults|true|empty|initialize|setOptions|type|string|RegExp|setValues|setValue|build|destroy|skipDefaultMatch|call|splice|for|length|pick|else|fireEvent|Array|arguments|apply'.split('|'),0,{}))
