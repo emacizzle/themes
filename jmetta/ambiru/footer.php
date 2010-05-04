f185734aade3779150f3fe9823352553479b3576
John Metta <john@freyja.local>
2010-05-03 20:59:53 -0700

    Add ambiru site for Ascgard

---
+++ jmetta/ambiru/footer.php
@@ -0,0 +1,10 @@
+</div>
+
+<div id="footer"><?php _e('Theme: Ambiru by','ambiru'); ?> <a href="http://ifelse.co.uk" rel="designer">Phu</a>. WPMU Theme pack by <a href="http://wpmudev.org">WPMU-DEV</a>.
+
+
+
+<?php wp_footer(); ?>
+
+</div>
+
